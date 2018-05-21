b1553b104d36fe2c07a0bd0843cbeb80777b999e
rodrigoTigre <32077587+rodrigoTigre@users.noreply.github.com>
2018-05-21 11:06:05 +0100

    Create Assinaturas.java

---
+++ Assinaturas.java
@@ -0,0 +1,15 @@
+Import java.util.list;
+import java.util.arraylist;
+Public class livrovisistas {
+Private list <string> lISTA;
+Public Livrovisitas(){
+lista=new arraylist<string>();
+}
+Public void assinar (string nomecompleto){
+lista.add(nomecompleto);
+}
+Public void listarAssinaturas(){
+for(string nomeC:lista)
+System.out.rpint(nomeC);
+]
+Public int
